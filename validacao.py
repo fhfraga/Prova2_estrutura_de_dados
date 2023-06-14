4c8cfb95b848a1212e5f5d3ac48fd924da525ec9
Fernando <f.henrique.fraga@gmail.com>
2023-06-13 22:36:03 -0300

    teste das funções novas

--- validacao.py
+++ validacao.py
@@ -15,10 +15,23 @@ sistema.cadastrar_pessoa("Fernando", "789456123",
 sistema.cadastrar_pessoa("Alessandra", "987654321",
                          "alessandra@gmail.com", "felino", "nenhuma")
 
+# Ver todos os animais
+print('Relatório de todos os animais')
+sistema.relatorio_todos_animais()
+print('----------------------\n')
+
 # Geração de relatório
+print('Geração de relatório')
 sistema.gerar_relatorio()
+print('----------------------\n')
 
 # Pesquisa por animal
+print('Pesquisa de animal com todas as caracteristicas')
 animal_encontrado = sistema.pesquisar_animal(
     "canino", "adulto", "marrom", "médio", "nenhuma")
 animal_encontrado
+print('----------------------\n')
+
+print('Pesquisa de animal pelo porte')
+print('Pesquisa binária recursiva')
+sistema.pesquisar_animal_pesquisa_binaria_recursiva('médio')
